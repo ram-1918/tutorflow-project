ea3510aa948d993a9ea586681ea11d7c8cba6072
ram-1918 <109194855+ram-1918@users.noreply.github.com>
2023-06-10 03:00:06 -0400

    Delete tutorfrontend directory

--- tutorfrontend/src/pages/CertbotResponse.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <h2>Certbot Response page</h2>
-    {{ $route.params.val }}
-</template>
--- tutorfrontend/src/pages/PageNotFound.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <base-header></base-header>
-    <no-content type="page-not-found" title="Sorry, PAGE NOT FOUND"></no-content>
-</template>
