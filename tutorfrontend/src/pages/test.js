d57c547280ff49f38267895a36ff49b69bde1fe6
Ram1918 <109194855+ram-1918@users.noreply.github.com>
2023-09-20 14:21:41 -0400

    Delete tutorfrontend directory

--- tutorfrontend/src/pages/test.js
+++
@@ -1,9 +0,0 @@
-function calcTime(d){
-    var today = new Date();
-    var milli = today - d;
-    console.log(today, d)
-    console.log('total seconds', milli, Math.floor(milli / (24*60*60*1000)));
-
-}
-var d = new Date(2023, 5, 8);
-calcTime(d);
--- tutorfrontend/src/store/auth.header.js
+++
@@ -1,8 +0,0 @@
-export default function authHeader() {
-    let user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.access) {
-      return { Authorization: 'JWT ' + user.access };
-    } else {
-      return {};
-    }
-  }
